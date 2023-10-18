7e9eac83dc5eaa6df3ba65177c5ab32139edb5cc
Fangjun Kuang <csukuangfj@gmail.com>
2023-10-18 14:44:55 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+#
+# Copyright      2022-2023  Xiaomi Corp.        (authors: Fangjun Kuang)
+#
+# See LICENSE for clarification regarding multiple authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# References:
+# https://gradio.app/docs/#dropdown
+
+import logging
+import os
+import uuid
+from pathlib import Path
+
+import gradio as gr
+
+from decode import decode
+from model import get_pretrained_model, get_vad, language_to_models
+
+title = "# Next-gen Kaldi: Text-to-speech (TTS)"
+
+description = """
+This space shows how to convert text to speech with Next-gen Kaldi.
+
+It is running on CPU within a docker container provided by Hugging Face.
+
+See more information by visiting the following links:
+
+- <https://github.com/k2-fsa/sherpa-onnx>
+
+If you want to deploy it locally, please see
+<https://k2-fsa.github.io/sherpa/>
+"""
+
+# css style is copied from
+# https://huggingface.co/spaces/alphacep/asr/blob/main/app.py#L113
+css = """
+.result {display:flex;flex-direction:column}
+.result_item {padding:15px;margin-bottom:8px;border-radius:15px;width:100%}
+.result_item_success {background-color:mediumaquamarine;color:white;align-self:start}
+.result_item_error {background-color:#ff7070;color:white;align-self:start}
+"""
+
+
+def update_model_dropdown(language: str):
+    if language in language_to_models:
+        choices = language_to_models[language]
+        return gr.Dropdown.update(choices=choices, value=choices[0])
+
+    raise ValueError(f"Unsupported language: {language}")
+
+
+def build_html_output(s: str, style: str = "result_item_success"):
+    return f"""
+    <div class='result'>
+        <div class='result_item {style}'>
+          {s}
+        </div>
+    </div>
+    """
+
+
+def process(language: str, repo_id: str, text: str, sid: str):
+    logging.info(f"Input text: {text}. sid: {sid}")
+    sid = int(sid)
+    tts = get_pretrained_model(repo_id)
+
+    start = time.time()
+    audio = tts.generate(text, sid=sid)
+    end = time.time()
+
+    if len(audio.samples) == 0:
+        raise ValueError(
+            "Error in generating audios. Please read previous error messages."
+        )
+
+    duration = len(audio.samples) / audio.sample_rate
+
+    elapsed_seconds = end - start
+    rtf = elapsed_seconds / duration
+
+    info = f"""
+    Wave duration  : {duration:.3f} s <br/>
+    Processing time: {elapsed_seconds:.3f} s <br/>
+    RTF: {elapsed_seconds:.3f}/{duration:.3f} = {rtf:.3f} <br/>
+    """
+
+    logging.info(info)
+    logging.info(f"\nrepo_id: {repo_id}\ntext: {text}")
+
+    filename = str(uuid.uuid4())
+    filename = f"{filename}.wav"
+    sf.write(
+        filename,
+        audio.samples,
+        samplerate=audio.sample_rate,
+        subtype="PCM_16",
+    )
+
+    return filename, build_html_output(info)
+
+
+demo = gr.Blocks(css=css)
+
+
+with demo:
+    gr.Markdown(title)
+    language_choices = list(language_to_models.keys())
+
+    language_radio = gr.Radio(
+        label="Language",
+        choices=language_choices,
+        value=language_choices[0],
+    )
+
+    model_dropdown = gr.Dropdown(
+        choices=language_to_models[language_choices[0]],
+        label="Select a model",
+        value=language_to_models[language_choices[0]][0],
+    )
+
+    language_radio.change(
+        update_model_dropdown,
+        inputs=language_radio,
+        outputs=model_dropdown,
+    )
+
+    with gr.Tabs():
+        with gr.TabItem("Please input your text"):
+            input_text = gr.Textbox(
+                label="Input text",
+                info="Your text",
+                lines=3,
+                placeholder="Please input your text here",
+            )
+
+            input_sid = gr.Textbox(
+                label="Speaker ID",
+                info="Speaker ID",
+                lines=1,
+                max_lines=1,
+                value="0",
+                placeholder="Speaker ID. Valid only for mult-speaker model",
+            )
+            input_button = gr.Button("Submit")
+
+            output_audio = gr.Audio(label="Output")
+
+            output_info = gr.HTML(label="Info")
+
+        input_button.click(
+            process,
+            inputs=[
+                language_radio,
+                model_dropdown,
+                input_text,
+                input_sid,
+            ],
+            outputs=[
+                output_audio,
+                output_info,
+            ],
+        )
+
+    gr.Markdown(description)
+
+if __name__ == "__main__":
+    formatter = "%(asctime)s %(levelname)s [%(filename)s:%(lineno)d] %(message)s"
+
+    logging.basicConfig(format=formatter, level=logging.INFO)
+
+    demo.launch()
---
+++ model.py
@@ -0,0 +1,136 @@
+# Copyright      2022-2023  Xiaomi Corp.        (authors: Fangjun Kuang)
+#
+# See LICENSE for clarification regarding multiple authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from functools import lru_cache
+
+import sherpa_onnx
+from huggingface_hub import hf_hub_download
+
+
+def get_file(
+    repo_id: str,
+    filename: str,
+    subfolder: str = ".",
+) -> str:
+    model_filename = hf_hub_download(
+        repo_id=repo_id,
+        filename=filename,
+        subfolder=subfolder,
+    )
+    return model_filename
+
+
+@lru_cache(maxsize=10)
+def _get_vits_vctk(repo_id: str) -> sherpa_onnx.OfflineTts:
+    assert repo_id == "csukuangfj/vits-vctk"
+
+    model = get_file(
+        repo_id=repo_id,
+        filename="vits-vctk.onnx",
+        subfolder=".",
+    )
+
+    lexicon = get_file(
+        repo_id=repo_id,
+        filename="lexicon.txt",
+        subfolder=".",
+    )
+
+    tokens = get_file(
+        repo_id=repo_id,
+        filename="tokens.txt",
+        subfolder=".",
+    )
+
+    tts_config = sherpa_onnx.OfflineTtsConfig(
+        model=sherpa_onnx.OfflineTtsModelConfig(
+            vits=sherpa_onnx.OfflineTtsVitsModelConfig(
+                model=model,
+                lexicon=lexicon,
+                tokens=tokens,
+            ),
+            provider="cpu",
+            debug=False,
+            num_threads=2,
+        )
+    )
+    tts = sherpa_onnx.OfflineTts(tts_config)
+
+    return tts
+
+
+@lru_cache(maxsize=10)
+def _get_vits_zh_aishell3(repo_id: str) -> sherpa_onnx.OfflineTts:
+    assert repo_id == "csukuangfj/vits-zh-aishell3"
+
+    model = get_file(
+        repo_id=repo_id,
+        filename="vits-aishell3.onnx",
+        subfolder=".",
+    )
+
+    lexicon = get_file(
+        repo_id=repo_id,
+        filename="lexicon.txt",
+        subfolder=".",
+    )
+
+    tokens = get_file(
+        repo_id=repo_id,
+        filename="tokens.txt",
+        subfolder=".",
+    )
+
+    tts_config = sherpa_onnx.OfflineTtsConfig(
+        model=sherpa_onnx.OfflineTtsModelConfig(
+            vits=sherpa_onnx.OfflineTtsVitsModelConfig(
+                model=model,
+                lexicon=lexicon,
+                tokens=tokens,
+            ),
+            provider="cpu",
+            debug=False,
+            num_threads=2,
+        )
+    )
+    tts = sherpa_onnx.OfflineTts(tts_config)
+
+    return tts
+
+
+@lru_cache(maxsize=10)
+def get_pretrained_model(repo_id: str) -> sherpa_onnx.OfflineTts:
+    if repo_id in chinese_models:
+        return chinese_models[repo_id](repo_id)
+    elif repo_id in english_models:
+        return english_models[repo_id](repo_id)
+    else:
+        raise ValueError(f"Unsupported repo_id: {repo_id}")
+
+
+chinese_models = {
+    "csukuangfj/vits-zh-aishell3": _get_vits_zh_aishell3,
+}
+
+english_models = {
+    "csukuangfj/vits-vctk": _get_vits_vctk,
+}
+
+
+language_to_models = {
+    "Chinese": list(chinese_models.keys()),
+    "English": list(english_models.keys()),
+}
